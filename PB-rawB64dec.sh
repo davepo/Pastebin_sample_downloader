c55b0429265ee44c8858fddbe8ee13dca54da840
davepo <davepo@gmail.com>
2018-07-29 09:46:29 -0400

    Update and rename raw2file.sh to PB-rawB64dec.sh

--- PB-rawB64dec.sh
+++ PB-rawB64dec.sh
@@ -3,5 +3,9 @@ echo -n "Enter PasteBin identifier (ie: for pastebin.com/12abAB, enter 12abAB): 
 read -e LOCATOR
 curl -X GET https://pastebin.com/raw/$LOCATOR | base64 -d > temp.malware
 sum=$(md5sum temp.malware)
-echo "${sum%% *}"
-mv temp.malware ${sum%% *}
+dt=$(date +"%y%m%d-%H%M")
+echo "Sample MD5: ${sum%% *}"
+echo "Current timestamp: ${dt%% *}"
+nm="${dt%% *}-${sum%% *}"
+mv temp.malware $nm
+echo "Sample saved as: $nm"
