01ea77916676bf814587b5f7e38b2c90321d6710
davepo <davepo@gmail.com>
2018-07-29 09:54:05 -0400

    Update PB-rawB64dec.sh

--- PB-rawB64dec.sh
+++ PB-rawB64dec.sh
@@ -4,6 +4,7 @@ read -e LOCATOR
 curl -X GET https://pastebin.com/raw/$LOCATOR | base64 -d > temp.malware
 sum=$(md5sum temp.malware)
 dt=$(date +"%y%m%d-%H%M")
+echo "Source URL: https://pastebin.com/raw/$LOCATOR"
 echo "Sample MD5: ${sum%% *}"
 echo "Current timestamp: ${dt%% *}"
 nm="${dt%% *}-${sum%% *}"
